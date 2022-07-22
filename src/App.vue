ffc424ec2b153f85e57b06a6917ed2c906b3aafb
SabyAndy <anderson.sabina32@gmail.com>
2022-07-22 17:13:19 +0000

    countdown_Timer

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div><img src="@/assets/bg-stars.svg" alt="" class="stars" /></div>
+    <div class="bodyContainer">
+      <Timer />
+    </div>
+
+    <div class="base">
+      <div><img src="@/assets/pattern-hills.svg" alt="" class="footerImage" /></div>
+      <div class="footer">
+        <div class="tabs">
+          <img
+            :src="tab.image"
+            alt=""
+            class="icon"
+            v-for="tab in footerTabs"
+            :key="tab.id"
+            :class="{ active: tab === activeTab }"
+            @click="() => (activeTab = tab)"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Timer from "@/components/AppTimer.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Timer },
+  data() {
+    return {
+      activeTab: "",
+      footerTabs: [
+        { id: "1", image: require("@/assets/facebook.svg") },
+        { id: "2", image: require("@/assets/Path.svg") },
+        { id: "3", image: require("../src/assets/Shape.svg") },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Red Hat Display";
+}
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #1d1e28;
+}
+.stars {
+  position: absolute;
+  top: 0;
+  max-width: 100%;
+}
+.footer {
+  display: flex;
+  justify-content: center;
+}
+.tabs {
+  position: absolute;
+  bottom: 100px;
+}
+.icon {
+  margin-right: 32px;
+}
+.icon.active {
+  background: #fb5e84;
+}
+.main {
+  background: #1d1e28;
+}
+.bodyContainer {
+  background: #1d1e28;
+  height: fit-content;
+  padding-bottom: 252px;
+}
+.footerImage {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+@media only screen and (max-width: 700px) {
+  .footerImage {
+    height: 168px;
+    top: 80%;
+    z-index: -1;
+  }
+  .bodyContainer {
+    padding-bottom: 100px;
+  }
+  .tabs {
+    position: absolute;
+    bottom: 48px;
+  }
 }
 </style>
---
+++ src/components/AppTimer.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <p class="header">We’re launching soon</p>
+
+    <div class="timeWrapper">
+      <!-- days wrap -->
+      <div class="card">
+        <div class="top">
+          <div class="upper"></div>
+        </div>
+
+        <div class="lower">
+          <div class="value">
+            <span class="timeIndicator">{{ days }}</span>
+          </div>
+        </div>
+        <p class="timeElement">DAYS</p>
+      </div>
+
+      <!-- hours -->
+      <div class="card">
+        <div class="top">
+          <div class="upper"></div>
+        </div>
+
+        <div class="lower">
+          <div class="value">
+            <span class="timeIndicator">{{ hours }}</span>
+          </div>
+        </div>
+        <p class="timeElement">HOURS</p>
+      </div>
+
+      <!-- minutes -->
+      <div class="card">
+        <div class="top">
+          <div class="upper"></div>
+        </div>
+
+        <div class="lower">
+          <div class="value">
+            <span class="timeIndicator">{{ minutes }}</span>
+          </div>
+        </div>
+        <p class="timeElement">MINUTES</p>
+      </div>
+
+      <!-- seconds -->
+      <div class="card">
+        <div class="top">
+          <div class="upper"></div>
+        </div>
+
+        <div class="lower">
+          <div class="value">
+            <span class="timeIndicator">{{ seconds }}</span>
+          </div>
+        </div>
+        <p class="timeElement">SECONDS</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Timer",
+  data() {
+    return {
+      days: "",
+      minutes: "",
+      hours: "",
+      seconds: "",
+    };
+  },
+  mounted() {
+    this.countdown();
+  },
+  methods: {
+    // formatNum(num) {
+    //   num < 10 ? "0" + num : num;
+    // },
+    countdown() {
+      const timer = setInterval(() => {
+        let lanuchDate = new Date("July 30, 2022 00: 00 : 00 ").getTime();
+        let now = new Date().getTime();
+        const difference = lanuchDate - now;
+
+        let seconds = 1000;
+        let minutes = seconds * 60;
+        let hours = minutes * 60;
+        let days = hours * 24;
+
+        let daysLeft = Math.floor(difference / days);
+        let hoursLeft = Math.floor((difference % days) / hours);
+        let minutesLeft = Math.floor((difference % hours) / minutes);
+        let secondsLeft = Math.floor((difference % minutes) / seconds);
+
+        this.days = daysLeft < 10 ? "0" + daysLeft : daysLeft;
+        this.hours = hoursLeft < 10 ? "0" + hoursLeft : hoursLeft;
+        this.minutes = minutesLeft < 10 ? "0" + minutesLeft : minutesLeft;
+        this.seconds = secondsLeft < 10 ? "0" + secondsLeft : secondsLeft;
+      }, 1000);
+      console.log(timer);
+      return timer;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  font-family: "Red Hat Text";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 22px;
+  line-height: 29px;
+  text-align: center;
+  letter-spacing: 7.5px;
+  text-transform: uppercase;
+  color: #ffffff;
+  margin: 132px 0 104px 0;
+}
+.timeWrapper {
+  display: flex;
+  justify-content: center;
+  gap: 0 32px;
+  flex-wrap: wrap;
+}
+.top {
+  background: #343650;
+  border-radius: 8px;
+}
+.upper {
+  background: #000000;
+  mix-blend-mode: normal;
+  opacity: 0.15;
+  border-radius: 0px 0px 8px 8px;
+  transform: matrix(1, 0, 0, -1, 0, 0);
+  width: 148px;
+  height: 70px;
+}
+.lower {
+  background: #343650;
+  border-radius: 8px;
+  width: 148px;
+  height: 70px;
+  position: relative;
+}
+.timeIndicator {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 80px;
+  line-height: 71px;
+  text-align: center;
+  letter-spacing: -2.4px;
+  color: #fb5e84;
+  text-align: center;
+  position: absolute;
+  top: -50%;
+  left: 20%;
+}
+.timeElement {
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 19px;
+  text-align: center;
+  letter-spacing: 5.91875px;
+  color: #8385a9;
+  margin-top: 16px;
+}
+@media only screen and (max-width: 700px) {
+  .header {
+    margin: 50px 0;
+  }
+}
+</style>
